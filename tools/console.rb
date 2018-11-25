d75dae61b665fef1e2e72bc862d7a94db5244c35
Lingy94 <j.lingyx@gmail.com>
2018-11-25 21:32:47 +0000

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -1,17 +1,16 @@
-require_relative '../config/environment.rb'
+require_relative '../environment.rb'
 
-<<<<<<< HEAD
-=======
 def reload
   load 'config/environment.rb'
 end
 
 jordan = Lifter.new("Jordan", 9999999)
+pat = Lifter.new("Pat", 500)
 twenty4 = Gym.new("24 Hour Fitness")
 hole_in_pocket = Membership.new(40, jordan, twenty4)
-
-
->>>>>>> parent of 39e6e66... skfjksdf
+new_pat = Membership.new(100, pat, twenty4)
+fit1 = Gyn.new("Fitness First")
+another = Membership.new(40, jon, fit1)
 
 
 binding.pry
