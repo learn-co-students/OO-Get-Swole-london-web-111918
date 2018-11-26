6365b00eb661271777d83815688a9b263d1bbcc4
Lingy94 <j.lingyx@gmail.com>
2018-11-26 08:57:59 +0000

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -9,7 +9,7 @@ pat = Lifter.new("Pat", 500)
 twenty4 = Gym.new("24 Hour Fitness")
 hole_in_pocket = Membership.new(40, jordan, twenty4)
 new_pat = Membership.new(100, pat, twenty4)
-fit1 = Gyn.new("Fitness First")
+fit1 = Gym.new("Fitness First")
 another = Membership.new(40, jon, fit1)
 
 
